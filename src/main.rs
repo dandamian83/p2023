eb7d4146a0897c5bb38ee6749800598f417a99d4
Dan Damian <arcassis@gmail.com>
2023-04-20 17:04:39 +0300

    Added exercise c

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 // use learning::{greet, exercise_c_simple_types};
-use learning::{exercise_e_ownership_and_references};
+use learning::{exercise_f_structs_traits};
 
 fn main() {
     // greet();
     // exercise_c_simple_types();
-    exercise_e_ownership_and_references();
+    // exercise_e_ownership_and_references();
+    exercise_f_structs_traits();
 }
